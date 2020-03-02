ebc2439ef89c8e85f0d9396c374b0e290992b79e
iceinto <iceinto@163.com>
2020-03-02 16:18:55 +0800

    因安全问题-需要升级框架内容到：
    Upgrade to Spring Framework 5.1.14
    Upgrade to Tomcat 9.0.31
    
    对应 Spring Boot 版本：
    
    https://github.com/spring-projects/spring-boot/releases/tag/v2.1.13.RELEASE
    https://github.com/spring-projects/spring-boot/releases/tag/v2.2.5.RELEASE
    
    对应 SpringCloud 版本：
    
    https://cloud.spring.io/spring-cloud-static/Greenwich.SR5/single/spring-cloud.html
    https://cloud.spring.io/spring-cloud-static/Hoxton.SR2/reference/html/spring-cloud.html

--- service-wx/src/main/java/org/iceinto/cloud/servicewx/ServiceWxApplication.java
+++ service-wx/src/main/java/org/iceinto/cloud/servicewx/ServiceWxApplication.java
@@ -6,12 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 
+ * @author iceinto
+ *
+ */
 @Configuration
 @EnableAutoConfiguration
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceWxApplication {
 
+	/**
+	 * @author iceinto
+	 * @param args
+	 */
     public static void main(String[] args) {
         SpringApplication.run(ServiceWxApplication.class, args);
     }
--- service-wx/src/main/java/org/iceinto/cloud/servicewx/controller/ComputeController.java
+++ service-wx/src/main/java/org/iceinto/cloud/servicewx/controller/ComputeController.java
@@ -5,14 +5,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 模拟测试
+ * @author iceinto
+ *
+ */
 @RestController
 public class ComputeController {
 
+	/**
+	 * @author iceinto
+	 * @param a
+	 * @param b
+	 * @return 两个数想加之和
+	 */
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String add(@RequestParam Integer a, @RequestParam Integer b) {
 
         Integer r = a + b;
-        //logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
         return "From Service-wx, Port:"  + " Result is " + r;
     }
 
